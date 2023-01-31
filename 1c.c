2c69452c0cbf63dc2a47ddc3c506a0c8c38a1f50
mads <anderen72@gmail.com>
2023-01-31 19:55:59 +0100

    e

---
+++ 1c.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <limits.h> //INT_MAX lives here
+
+
+int main()
+{
+    int rows = 5;
+    int cols = 7;
+    float *row_array;
+    float *col_array;
+    row_array = malloc(rows*sizeof(float));
+    array_array =
+
+}
