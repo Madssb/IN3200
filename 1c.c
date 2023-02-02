e90349ec92e271970cf0d50adef6b499a088b3c4
mads <anderen72@gmail.com>
2023-02-02 12:13:03 +0100

    jau

--- 1c.c
+++ 1c.c
@@ -4,13 +4,35 @@
 #include <limits.h> //INT_MAX lives here
 
 
+int *initialize_matrix_random_elements_row_first(int rows,int cols)
+{
+for(index1=0; index1 < rows; index1++)
+    {
+        for(index2=0; index2 < cols; index2++)
+        {
+            matrix[index1][index2] = rand() % 100;
+        }
+    }
+    return matrix_array;
+}
+
+int *initialize_matrix_random_elements_col_first(int rows,int cols)
+{
+for(index2=0; index2 < col; index2++)
+    {
+        for(index1=0; index1 < row; index1++)
+        {
+            matrix[index1][index2] = rand() % 100;
+        }
+    }
+    return matrix_array;
+}
 int main()
 {
     int rows = 5;
     int cols = 7;
-    float *row_array;
-    float *col_array;
-    row_array = malloc(rows*sizeof(float));
-    array_array =
+    int* matrix1 = initialize_matrix_random_elements_row_first(rows,cols);
+    int* matrix2 = initialize_matrix_random_elements_col_first(rows,cols);
+
 
 }
