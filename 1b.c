86add7445ba146949a5c920b9827aaa9bb6a9e84
Mads <madssb@uio.no>
2023-02-05 12:11:21 +0100

    added docstrings to 1b functions

--- 1b.c
+++ 1b.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <limits.h> //INT_MAX lives here
-
+#include <math.h> //INFINITY lives here
 /*
  * Author: Mads S. Balto
  * Date: 04/02/23
@@ -12,14 +11,15 @@
  */
 
 /**
- * @brief Finds the largest element within a provided 1D array.
- * @param array Pointer to one-dimensional array, consisting of (unsigned) integers. In current implementation, function is not tested to handle signed integers.
- * @param array_size Amount of elements contained within provided array.
- * @return The largest integer found within provided array. 
+ * @brief Finds the largest element within a provided one-dimensional array.
+ * 
+ * @param array Pointer to one-dimensional array consisting of floats.
+ * @param array_size Amount of floats contained within provided array. If array_size exceeds actual array size, non-allocated memory will be attempted accessed. If provided array size falls short of actual array size, the largest element may not be found. 
+ * @return Largest float found within provided array. if empty array is provided, returns negative infinity.
  */
 double find_max_array_element(double *array, size_t array_size)
 {
-    double largest_element = 0.;
+    double largest_element = -INFINITY;
     for(size_t index = 0; index < array_size; index++)
     {
         if (array[index] > largest_element)
@@ -29,20 +29,30 @@ double find_max_array_element(double *array, size_t array_size)
     }
     return largest_element;
 }
-
+/**
+ * @brief Finds the smallest element within a provided one-dimensional array.
+ * 
+ * @param array Pointer to one-dimensional array consisting of floats.
+ * @param array_size Amount of floating point values contained within provided array. If array_size exceeds actual array size, non-allocated memory will be attempted accessed. If array_size falls short of actual array size, the smallest element may not be found. 
+ * @return Smallest float foudn within provided array. If empty array is provided, returns infinity
+ */
 double find_min_array_element(double *array, size_t array_size)
 {
-    double smallest_element = (double)INT_MAX;
+    double smallest_element = INFINITY;
     for(size_t index = 0; index < array_size; index++)
     {
-        if (array[index] < smallest_element)
+        if (array[index] < smallest_element)    
         {
             smallest_element = array[index];
         }
     }
     return smallest_element;
 }
-
+/**
+ * @brief generates a pseudo-random double, based on calls to a pseudo RNG method for integers. 
+ * 
+ * @return double of pseudo-random nature. May not be uniformly distributed. 
+ */
 double double_pseudo_rng()
 {
     double rand_double = (double)rand()/(double)RAND_MAX;
@@ -53,6 +63,12 @@ double double_pseudo_rng()
     rand_double *= rand();
     return rand_double;
 }
+/**
+ * @brief Initializes one-dimensional array of fixed length with pseudo-random-like doubles.
+ * 
+ * @param array_size Amount of floating point values contained within provided array.
+ * @return Pointer to array consisting of pseudo-random-like doubles.
+ */
 double * initialize_array_random_elements(size_t array_size)
 {
     srand((unsigned int)time(NULL)); // seed the random number generator to known value
@@ -65,7 +81,12 @@ double * initialize_array_random_elements(size_t array_size)
     }
     return array;
 }
-
+/**
+ * @brief prints the elements of a provided one-dimensional array to the terminal. 
+ * 
+ * @param array Pointer to one-dimensional array with double elements.
+ * @param array_size expected amount of elements stored in array. if array_size exceeds actual array size, dereferences an invalid pointer. If array_size falls short of actual array size, function does not print all elements of array.
+ */
 void print_array_contents(double *array,size_t array_size)
 {
     for (size_t index = 0; index < array_size; index++)
